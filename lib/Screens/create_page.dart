c2500bce610e3cd5398c290fa0e4e1f0f6622d0f
Sushant Neupane <nsushaant72@gmail.com>
2025-06-09 21:46:12 +0545

    updated ui using custom text field

--- lib/Screens/create_page.dart
+++ lib/Screens/create_page.dart
@@ -1,37 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Widgets/custom_textfield.dart';
 
 class CreateNote extends StatelessWidget {
   const CreateNote({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final myBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(10),
-      borderSide: BorderSide(color: Colors.white),
-    );
+    final TextEditingController textEditingControllerTitle =
+        TextEditingController();
+    final TextEditingController textEditingControllerDescription =
+        TextEditingController();
+
     return Scaffold(
       appBar: AppBar(title: Text("Create Notes"), centerTitle: true),
       body: Padding(
         padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
-            TextField(
-              decoration: InputDecoration(
-                focusedBorder: myBorder,
-                enabledBorder: myBorder,
-              ),
+            CustomTextfield(
+              textEditingController: textEditingControllerTitle,
+              hintText: "Title",
+            ),
+            CustomTextfield(
+              textEditingController: textEditingControllerDescription,
+              hintText: "Description",
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => CreateNote()),
-          );
+          String title = textEditingControllerTitle.text;
+          String description = textEditingControllerDescription.text;
+
+          final Map<String, String> note = {
+            "title": title,
+            "description": description,
+
+            "CreatedAt": DateTime.now().toString(),
+          };
+          print(note);
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/Widgets/custom_textfield.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomTextfield extends StatelessWidget {
+  final String hintText;
+  final TextEditingController textEditingController;
+  const CustomTextfield({
+    super.key,
+    required this.textEditingController,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final myBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(color: Colors.white),
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10,),
+      child: TextField(
+        controller: textEditingController,
+        decoration: InputDecoration(
+          focusedBorder: myBorder,
+          enabledBorder: myBorder,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
